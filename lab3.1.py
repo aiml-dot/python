b90765024ddc69375b41cdb80981831b15923fdb
aiml-dot <sanchitanalawade844@gmail.com>
2026-08-17 10:50:17 +0530

    Add files via upload

---
+++ lab3.1.py
@@ -0,0 +1,12 @@
+print("-----------SCHOLORSHIP ELIGIBLITY CHEAKER----------")
+
+name = input("Enetr Your Name : ")
+age = int(input("Enter your age : "))
+income = float(input("Enter your annual family income ($) : "))
+caste = input("Enter your caste(General / OBC / SC / ST ) : ")
+
+if age < 25 and income < 300000:
+    print(" \nCONGRATULATIONS!")
+    print(name, " You Are Eligible For Scholorship ")
+else:
+        print(name," You Are Not Eligible For Scholorship ")  
