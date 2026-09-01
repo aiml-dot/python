4fbbc782f68dd64c4cbcb8ef2d71bbde06a54074
aiml-dot <sanchitanalawade844@gmail.com>
2026-09-01 12:33:20 +0530

    Update lab3.1.py

--- lab3.1.py
+++ lab3.1.py
@@ -3,7 +3,7 @@ print("-----------SCHOLORSHIP ELIGIBLITY CHEAKER----------")
 name = input("Enetr Your Name : ")
 age = int(input("Enter your age : "))
 income = float(input("Enter your annual family income ($) : "))
-caste = input("Enter your caste(General / OBC / SC / ST ) : ")
+caste = input("Enter your caste(OBC / SC / ST ) : ")
 
 if age < 25 and income < 300000:
     print(" \nCONGRATULATIONS!")
